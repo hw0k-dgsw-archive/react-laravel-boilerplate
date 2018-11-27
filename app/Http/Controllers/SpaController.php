c5242b312212eb5af16b6ed9cc6b9b7571b57417
남현욱 <hw0knam@gmail.com>
2018-11-27 20:07:24 +0900

    Update SpaController.php

--- app/Http/Controllers/SpaController.php
+++ app/Http/Controllers/SpaController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 
 class SpaController extends Controller
 {
-    public function __constructor()
-    {
-        parent::__constructor();
-    }
-    
     public function __invoke()
     {
         return view('spa');
